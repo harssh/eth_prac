7c002b922133c5ce7ff8b224c2395577d79c9055
Harssh <Harsshshrivastava@sullivancotter.com>
2017-06-30 19:21:13 -0500

    Added provider user and mortal contract

---
+++ demo.sol
@@ -0,0 +1,89 @@
+/**
+ * This contract does this and that...
+ */
+contract mortal {
+  address public owner;
+
+
+  function mortal() {
+    owner = msg.sender;
+  }
+
+  function kill() {
+    suicide(owner);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * This contract does this and that...
+ */
+contract User {
+
+  string public userName;
+
+  function User (string _name) {
+    userName = _name;
+  }
+}
+
+
+
+----------
+
+
+/**
+ * This contract does this and that...
+ */
+contract mortal {
+
+  address public owner;
+  function mortal()  {
+    owner = msg.sender;
+  }
+
+  function kill() {
+    suicide(owner)
+
+  }
+}
+
+
+/**
+ * This contract does this and that...
+ */
+contract Userm is mortal  {
+
+  string public userName;
+
+  function Userm  (string _name) {
+    userName = _name;
+  }
+}
+
+
+/**
+ * This contract does this and that...
+ */
+contract Providerm is mortal {
+
+  string public providerName;
+  function Providerm  (string _name) {
+    providerName = _name;
+  }
+}
+
+
+
+
+
